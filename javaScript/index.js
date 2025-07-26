358661aa88079baecb30d5201fc346b73c889f9b
Santiago Mejia Ocampo <santimejiaok@gmail.com>
2025-07-26 16:56:05 -0500

    download phaser and start project

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,7 +1,24 @@
-const canvas = document.getElementById('gameCanvas')
-const ctx = canvas.getContext('2d')
-canvas.width = 800
-canvas.height = 600
+const config = {
+    type: Phaser.CANVAS,
+    width: 256,
+    height: 244,
+    backgroundColor: 'white',
+    parent: 'gameCanvas',
+    scene: {
+        preload, 
+        create,
+        update
+    }
+}
 
-ctx.fillStyle = 'black'
-ctx.fillRect(0, 0, canvas.width, canvas.height)
+function preload() {
+console.log('preload')
+}
+
+function create() {
+console.log('create')
+}
+
+function update() {
+console.log('update')
+}
