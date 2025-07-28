d05c0721f179cc4fa74968afff2c9d4cf4ba5ebf
Santiago Mejia Ocampo <santimejiaok@gmail.com>
2025-07-27 22:37:50 -0500

    mario move and sprites

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,24 +1,40 @@
 const config = {
-    type: Phaser.CANVAS,
-    width: 256,
-    height: 244,
-    backgroundColor: 'white',
-    parent: 'gameCanvas',
-    scene: {
-        preload, 
-        create,
-        update
-    }
+  type: Phaser.AUTO,
+  width: 256,
+  height: 244,
+  backgroundColor: '#049cd8',
+  parent: 'gameCanvas',
+  scene: {
+    preload,
+    create,
+    update,
+  },
 }
 
+new Phaser.Game(config)
+
 function preload() {
-console.log('preload')
+  this.load.image('cloud1', 'assets/scenery/overworld/cloud1.png')
+
+  this.load.spritesheet('mario', 'assets/entities/mario.png', { frameWidth: 18, frameHeight: 16 })
+
+  this.load.image('floorBricks', 'assets/scenery/overworld/floorbricks.png')
 }
 
 function create() {
-console.log('create')
+  this.add.image(0, 0, 'cloud1').setOrigin(0, 0).setScale(0.15)
+
+  this.add.tileSprite(0, config.height - 32, config.width, 32, 'floorBricks').setOrigin(0,0)
+
+  this.mario = this.add.sprite(50, 210, 'mario').setOrigin(0, 1)
+
+  this.keys = this.input.keyboard.createCursorKeys()
 }
 
 function update() {
-console.log('update')
+  if (this.keys.left.isDown) {
+    this.mario.x -= 2
+  } else if (this.keys.right.isDown) {
+    this.mario.x += 2
+  }
 }
